550bb41143503885981f0fb1e7d007dd0af5462c
Mydachekc <97265858+Mydachekc@users.noreply.github.com>
2022-01-24 22:57:13 +0200

    Release V 0.3 FIXED ALL
    Yeah i do it need just some times
    Telegram @mydachek
    Version 0.3 FIXED ALL
    If you find bug or something similar write on telegram or git good time of day!

--- [_OTHER_]/[_SOURCE_]/[_ABOUT-VNTU_].bat
+++ [_OTHER_]/[_SOURCE_]/[_ABOUT-VNTU_].bat
@@ -14,8 +14,8 @@ Set /p choice="Your choice: "
 Echo.
 if "%choice%"=="1" goto m1
 if "%choice%"=="2" (start "" "C:\Users\%username%\Desktop\1AKIT-21-main\[_OTHER_]\[_UPDATES_]\[_LATEST UPDATE_].txt")
-if "%choice%"=="3" (start "" "C:\Users\%username%\[_INFO_]\[_JET_]\[_OTHER_]\[_INFO_]\[_ABOUT-PROGRAM_].txt")
-if "%choice%"=="4" (Echo To chek the source go to folder "[_JET_]\[_OTHER_]\[_SOURCE_]")
+if "%choice%"=="3" (start "" "C:\Users\%username%\Desktop\1AKIT-21-main\[_OTHER_]\[_INFO_]\[_ABOUT-PROGRAM_].txt")
+if "%choice%"=="4" (Echo To chek the source go to folder "AKIT-21-main\[_OTHER_]\[_SOURCE_]")
 if "%choice%"=="0" goto end
 Echo.
 Echo Wrong selection, try again...
@@ -26,21 +26,21 @@ goto m0
 :m1
 Echo Select subject:
 Echo.
-Echo ENTER 5 - [AUTOMIZE]
-Echo ENTER 6 - [FIZIK]
-Echo ENTER 7 - [MATH]
-Echo ENTER 8 - [CODE]
-Echo ENTER 9 - [ON TEST...]
+Echo ENTER 1 - [AUTOMIZE]
+Echo ENTER 2 - [FIZIK]
+Echo ENTER 3 - [MATH]
+Echo ENTER 4 - [CODE]
+Echo ENTER 5 - [Eng]
 Echo ENTER 0 - [RERURN]
 echo.
 
 Set /p choice="Your choice: "
 Echo.
-if "%choice%"=="5" goto m1.1
-if "%choice%"=="6" goto m1.2
-if "%choice%"=="7" goto m1.3
-if "%choice%"=="8" goto m1.4
-if "%choice%"=="9" (Echo go to folder "[_JET_]\[_OTHER_]\[_SOURCE_]")
+if "%choice%"=="1" goto m1.1
+if "%choice%"=="2" goto m1.2
+if "%choice%"=="3" goto m1.3
+if "%choice%"=="4" goto m1.4
+if "%choice%"=="5" goto m1.5
 if "%choice%"=="0" goto m0
 Echo.
 Echo Wrong selection, try again...
@@ -146,6 +146,27 @@ Echo.
 Echo.
 goto m1.4
 
+:m1.5
+Echo [SUBJECT ENG] select home works:
+Echo.
+Echo ENTER 1 - [Home Task 1]
+Echo ENTER 2 - [Home Task 2]
+Echo ENTER 3 - [Home Task 3]
+Echo ENTER 0 - [RERURN]
+echo.
+
+Set /p choice="Choose home task: "
+Echo.
+if not defined choice goto m1.5
+if "%choice%"=="1" (start "" "C:\Users\%username%\Desktop\1AKIT-21-main\[_ENG_]\[_WORK_]\Home1.txt")
+if "%choice%"=="2" (start "" "C:\Users\%username%\Desktop\1AKIT-21-main\[_ENG_]\[_WORK_]\Home2.txt")
+if "%choice%"=="3" (start "" "C:\Users\%username%\Desktop\1AKIT-21-main\[_ENG_]\[_WORK_]\Home3.txt")
+if "%choice%"=="0" goto m1
+Echo.
+Echo Wrong selection, try again...
+Echo.
+Echo.
+goto m1.5
 
 :end
 pause
